2adc19b676a2a6a4b77ad0520e5772893d5f181f
Mikey Bradley <mwbradley@ksu.edu>
2026-02-16 13:06:11 -0600

    FCFS started

--- src/process_scheduling.c
+++ src/process_scheduling.c
@@ -18,11 +18,49 @@ void virtual_cpu(ProcessControlBlock_t *process_control_block)
 	--process_control_block->remaining_burst_time;
 }
 
+
+//https://www.geeksforgeeks.org/dsa/first-come-first-serve-cpu-scheduling-non-preemptive/ for help
 bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result) 
 {
-	UNUSED(ready_queue);
-	UNUSED(result);
-	return false;
+	if (ready_queue == NULL || result == NULL) return false;
+
+	size_t processes = dyn_array_data_size(ready_queue);
+	if (processes == 0) {
+		result->average_waiting_time = 0;
+		result->average_turnaround_time = 0;
+		return true;
+	}
+	
+	float cur_time = 0; // tracks where we are currently at
+	float avg_waiting_time = 0; 
+	float avg_turnaround_time = 0;
+	unsigned long total_turnaround_time = 0;
+
+	for (size_t i = 0; i < processes; i++) {
+		ProcessControlBlock_t *pcb = (ProcessControlBlock_t*)dyn_array_at(ready_queue, i); // puts process i at pcb
+
+		if (cur_time < pcb->arrival) {
+			cur_time = pcb->arrival;
+		}
+
+		// found calculation on geeksforgeeks
+		float waiting_time = cur_time - pcb->arrival;
+		float completion_time = cur_time + pcb->remaining_burst_time;
+		float turnaround_time = completion_time - pcb->arrival;
+
+		// we calculate averages outside the for loop
+		avg_waiting_time += waiting_time;
+		avg_turnaround_time += turnaround_time;
+
+		// adds completion time for each process
+		total_turnaround_time += completion_time;
+	}
+
+	result->average_waiting_time = avg_waiting_time / processes;
+	result->average_turnaround_time = avg_turnaround_time / processes;
+	result->total_run_time = total_turnaround_time;
+	
+	return true;
 }
 
 bool shortest_job_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
